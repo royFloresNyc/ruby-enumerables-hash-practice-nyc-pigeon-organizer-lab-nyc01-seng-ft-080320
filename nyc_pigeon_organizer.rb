c3f0a8a16edf86496bb1ea71607bff2709d67ac3
Roy  Flores <royfloresnyc@gmail.com>
2020-07-21 19:17:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,5 +7,9 @@ def nyc_pigeon_organizer(data)
 end
 
 def pigeon_name_getter(collection)
-  collection.map do |
+  pigeon_names = []
+  collection.each do |key, value|
+    value.each { |name| pigeon_names << name }
+  end
+  pigeon_names
 end
