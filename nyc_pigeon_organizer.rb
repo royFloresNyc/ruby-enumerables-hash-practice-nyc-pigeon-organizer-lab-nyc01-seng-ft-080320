7337b5eb4bda2e2051434ba52a02dc80683d87cc
Roy  Flores <royfloresnyc@gmail.com>
2020-07-21 20:17:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #get and set names of pigeons as key in a result hash
+  pigeon_names = pigeon_name_getter(data[:lives])
+  organized_hash = pigeon_name_key_setter(pigeon_names)
+  
+  #iterate over input hash
+  data.each do |attribute, value|
+    value.each do |sub_attribute, array_of_names|
+      array_of_names.each do |name|
+        if organized_hash[name].has_key?(attribute)
+          organized_hash[name][attribute] << sub_attribute.to_s
+        else
+          organized_hash[name][attribute] = [sub_attribute.to_s]
+        end
+      end
+    end  
+  end 
+  
+  organized_hash
+end
+
+#helper method (getter)
+def pigeon_name_getter(collection)
+  pigeon_names = []
+  collection.each do |key, value|
+    pigeon_names.concat(value)
+  end
+  pigeon_names
+end
+
+#helper method (setter)
+def pigeon_name_key_setter(array)
+  result_hash = {}
+  array.each do |name|
+    result_hash[name] = {}
+  end
+  result_hash
 end
